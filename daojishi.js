ce40a444e25c393a5573e02a44dfe6f7d5a9c276
HelloWorld <zxhycxq@users.noreply.github.com>
2016-03-04 20:30:36 +0800

    Update daojishi.js

--- daojishi.js
+++ daojishi.js
@@ -1,6 +1,6 @@
 window.onload=function(){
 	var curtime=new Date(),
 			endtime=new Date("2016,3,31"),
-			lefttime=Math.ceil((endtime.getTime()-curtime.getTime())/(24*60*60*1000));
+			lefttime=Math.ceil((endtime.getTime()-curtime.getTime())/(24*60*60*1000));//ceil向上取整
 	document.getElementById("timeShow").innerHTML="距离  "+endtime+"  仅剩下"+lefttime+"  天";
 	}
